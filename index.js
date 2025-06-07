8d43d638b7a320a73aca425121e47315786413f2
alex kamau <alex.kamau1@student.moringaschool.com>
2025-06-07 13:00:09 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
-// Write your solution here!
+// index.js
+
+const cats = ["Milo", "Otis", "Garfield"];
+
+// Destructive functions - modify the original cats array
+
+function destructivelyAppendCat(name) {
+  cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+  cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+}
+
+// Non-destructive functions - return new arrays, cats array unchanged
+
+function appendCat(name) {
+  return [...cats, name];
+}
+
+function prependCat(name) {
+  return [name, ...cats];
+}
+
+function removeLastCat() {
+  return cats.slice(0, cats.length - 1);
+}
+
+function removeFirstCat() {
+  return cats.slice(1);
+}
+
+module.exports = {
+  cats,
+  destructivelyAppendCat,
+  destructivelyPrependCat,
+  destructivelyRemoveLastCat,
+  destructivelyRemoveFirstCat,
+  appendCat,
+  prependCat,
+  removeLastCat,
+  removeFirstCat,
+};
